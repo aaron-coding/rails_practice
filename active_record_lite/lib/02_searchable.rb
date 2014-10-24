abfe41f78c67797f41c79c49f8496f34b40ac3f5
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-10-24 16:21:13 -0700

    Add Searchable

--- active_record_lite/lib/02_searchable.rb
+++ active_record_lite/lib/02_searchable.rb
@@ -2,11 +2,29 @@ require_relative 'db_connection'
 require_relative '01_sql_object'
 
 module Searchable
+  
   def where(params)
-    # ...
+    where_line = ""
+    where_line << params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+    results = DBConnection.execute(<<-SQL, *params.values)
+        SELECT
+          *
+        FROM
+          #{table_name}
+        WHERE 
+          #{where_line}
+        SQL
+        p results
+    all_results = []    
+    results.each do |result|
+      all_results << self.new(result)
+    end
+    all_results
   end
+  
+  
 end
 
 class SQLObject
-  # Mixin Searchable here...
+    extend Searchable
 end
