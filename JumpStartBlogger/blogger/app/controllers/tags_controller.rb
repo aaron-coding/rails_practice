bc97fc6aba885f62880fa5da0e0ab56384a39353
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-10-26 10:57:59 -0700

    Finish Tags delete functionality

--- JumpStartBlogger/blogger/app/controllers/tags_controller.rb
+++ JumpStartBlogger/blogger/app/controllers/tags_controller.rb
@@ -6,4 +6,11 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+  
+  def destroy
+    Tag.find(params[:id]).destroy
+    flash.notice = "Tag Deleted!"
+    
+    redirect_to tags_path
+  end
 end
