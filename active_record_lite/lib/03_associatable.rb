25d06b81698e7a7004992947799edf5a80ead723
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-10-26 12:16:24 -0700

    Finish Associatable

--- active_record_lite/lib/03_associatable.rb
+++ active_record_lite/lib/03_associatable.rb
@@ -10,91 +10,82 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    
-     # define_method("foreign_key") do
-    #    "#{name.to_s.singularize}_id".to_sym
-    #  end
-    #
-    #  define_method("primary_key") do
-    #    :id
-    #  end
-    #
-    #  define_method("class_name") do
-    #    name.to_s.camelcase
-    #  end
-    
     @foreign_key = options[:foreign_key] || 
-       "#{name.to_s.singularize}_id".to_sym
-     
+       "#{name}_id".to_sym
     @primary_key = options[:primary_key] || :id
-       
-       
     @class_name = options[:class_name] || 
        name.to_s.camelcase
-       #
-     # def foreign_key(name = options[foreign_key])
-     #   
-     # end
-     #
-     # def primary_key
-     #   
-     # end
-     #
-     # def class_name(name = options[class_name])
-     #   
-     # end
-     #
-    #
-    # if options[foreign_key].nil?
-    #   options[foreign_key] = "#{name.to_s.singularize}_id".to_sym
-    # end
-    #
-    # if options[primary_key].nil?
-    #   options[primary_key] = :id
-    # end
-    #
-    # if options[class_name].nil?
-    #   options[class_name] = name.to_s.camelcase
-    # end
-    #
-    # options
+
   end
-  
-  
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     
+    @foreign_key = options[:foreign_key] || 
+       "#{self_class_name.to_s.singularize.downcase}_id".to_sym
+     
+    @primary_key = options[:primary_key] || :id
+       
+       
+    @class_name = options[:class_name] || 
+       name.to_s.singularize.camelcase
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    
+    # options = BelongsToOptions.new(name, options)
+    #
+    # define_method("#{name}") do
+    #   options.send(foreign_key)
+    # end
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+        define_method(name) do
+          options = self.class.assoc_options[name]
+
+          key_val = self.send(options.foreign_key)
+          options
+            .model_class
+            .where(options.primary_key => key_val)
+            .first
+        end
   end
 
   def has_many(name, options = {})
-    # ...
+     self.assoc_options[name] =
+      HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options
+        .model_class
+        .where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+       @assoc_options
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 
 end
