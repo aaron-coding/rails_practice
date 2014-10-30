dbab44c619e92d90a8d91ad99b8f957491ce409d
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-10-30 11:04:55 -0700

    Add minor fixes

--- active_record_lite/lib/03_associatable.rb
+++ active_record_lite/lib/03_associatable.rb
@@ -1,7 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
 
-# Phase IIIa
 class AssocOptions
   attr_accessor(
     :foreign_key,
@@ -22,6 +21,7 @@ class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     @foreign_key = options[:foreign_key] || 
        "#{name}_id".to_sym
+       
     @primary_key = options[:primary_key] || :id
     @class_name = options[:class_name] || 
        name.to_s.camelcase
@@ -33,10 +33,9 @@ class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     
     @foreign_key = options[:foreign_key] || 
-       "#{self_class_name.to_s.singularize.downcase}_id".to_sym
+       "#{self_class_name.to_s.downcase}_id".to_sym
      
     @primary_key = options[:primary_key] || :id
-       
        
     @class_name = options[:class_name] || 
        name.to_s.singularize.camelcase
@@ -44,39 +43,39 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
     
-    # options = BelongsToOptions.new(name, options)
-    #
-    # define_method("#{name}") do
-    #   options.send(foreign_key)
-    # end
-    self.assoc_options[name] = BelongsToOptions.new(name, options)
 
-        define_method(name) do
-          options = self.class.assoc_options[name]
+    define_method(name) do
+      options = BelongsToOptions.new(name, options)
 
-          key_val = self.send(options.foreign_key)
-          options
-            .model_class
-            .where(options.primary_key => key_val)
-            .first
-        end
+      val_of_our_foreign_key = self.send(options.foreign_key)
+      hashes = DBConnection.execute(<<-SQL, val_of_our_foreign_key)
+      SELECT *
+      FROM #{options.table_name}
+      WHERE #{options.primary_key} = ?
+      LIMIT 1
+      SQL
+      
+      hashes.map{ |data| options.model_class.new(data) }.first
+    end
+
   end
 
   def has_many(name, options = {})
-     self.assoc_options[name] =
-      HasManyOptions.new(name, self.name, options)
-
-    define_method(name) do
-      options = self.class.assoc_options[name]
-
-      key_val = self.send(options.primary_key)
-      options
-        .model_class
-        .where(options.foreign_key => key_val)
+    define_method(name) do                           
+      options = HasManyOptions.new(name, self.class.name, options)
+      val_of_our_self_id = self.send(options.primary_key)
+       hashes = DBConnection.execute(<<-SQL, val_of_our_self_id)
+       SELECT *
+       FROM #{options.table_name}
+       WHERE #{options.foreign_key} = ?
+       SQL
+      
+      hashes.map { |data| options.model_class.new(data) }
     end
+    
+
   end
 
   def assoc_options
