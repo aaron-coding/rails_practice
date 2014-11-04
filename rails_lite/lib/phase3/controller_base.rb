7bfd5b356d59f02adb279a41e5e72920e2c84199
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-11-04 14:16:27 -0800

    Add render content and render template functions

--- rails_lite/lib/phase3/controller_base.rb
+++ rails_lite/lib/phase3/controller_base.rb
@@ -1,12 +1,18 @@
 require_relative '../phase2/controller_base'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 
 module Phase3
   class ControllerBase < Phase2::ControllerBase
-    # use ERB and binding to evaluate templates
-    # pass the rendered html to render_content
     def render(template_name)
+      # This looks at the file called "views/CONTROLLER/template_name.html.erb"
+      # it will create that into an instance of ERB and then render it with
+      # all instance variables available.
+      template = ERB.new(
+      File.read("views/#{self.class.to_s.underscore}/#{template_name}.html.erb")
+      )
+      render_content(template.result(binding), "text/html")
     end
   end
 end
