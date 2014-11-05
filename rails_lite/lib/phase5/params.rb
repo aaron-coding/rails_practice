58c31b2d53659d4e9a45f5d59f233f5f795a33c2
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-11-04 17:12:25 -0800

    Add deep parsing of query strings

--- rails_lite/lib/phase5/params.rb
+++ rails_lite/lib/phase5/params.rb
@@ -6,13 +6,26 @@ module Phase5
     # 1. query string
     # 2. post body
     # 3. route params
-    #
+    
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+       @params = {}
+       if req.query_string
+         parse_www_encoded_form(req.query_string)
+       end
+       if req.body
+         parse_www_encoded_form(req.body)
+       end
+       if !(route_params.empty?)
+         route_params.each do |k,v|
+           @params[k] = v
+         end
+       end
     end
 
     def [](key)
+      @params[key.to_s] #this makes symbols process identically to strings
     end
 
     def to_s
@@ -28,11 +41,34 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      _query = URI.decode_www_form(www_encoded_form)
+      _query.each do |key, val|
+        if parse_key(key).count == 1
+          @params[key] = val
+        else
+          nested_hash = {}
+          all_keys = parse_key(key)
+          first = true
+          all_keys.reverse.each do |key|  # build a nested hash
+            if first                      # First assign value
+              nested_hash[key] = val      
+            else
+              new_hash = {}               # put it into a nested hash
+              new_hash[key] = nested_hash # until iterate through all 
+              nested_hash = new_hash      # parts
+            end
+            first = false
+          end
+          @params = nested_hash
+        end
+      end
+      
     end
 
-    # this should return an array
+    # this returns an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
